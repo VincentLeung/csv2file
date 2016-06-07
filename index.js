8129da8c1dfd448f9fefece8d66ebe590d423a25
Holger Bartel <h@foobartel.com>
2016-06-07 19:00:29 +0800

    uninstall and remove sanitize-filename

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var fs = require('fs');
 var path = require('path');
 var rmdir = require('rmdir');
 var dateFormat = require('dateformat');
-var sanitize = require("sanitize-filename");
 var unidecode = require('unidecode');
 var slug = require('slug');
 
